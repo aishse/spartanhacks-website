4d4604c274988cb976246d4bdba33d66f163e500
aishse <email.friendme@gmail.com>
2024-06-25 00:08:47 -0700

    basic styling

---
+++ sh-site/src/app/Hero.tsx
@@ -0,0 +1,12 @@
+
+
+export default function Hero () {
+  return (
+    <div className="justify-items-center text-center">
+        <h1 className="text-5xl lg:text-7xl">{"<Spartan "}</h1>
+        <h1 className="text-5xl lg:text-7xl">{"Hacks/> "}</h1>
+    </div>
+  );
+}
+
+
